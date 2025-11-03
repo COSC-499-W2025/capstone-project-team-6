e2f28197e71adba711b4a0caad4de542e544e3d0
HarjotSahota <93726984+HarjotSahota@users.noreply.github.com>
2025-11-02 16:05:11 -0800

    Resolved Merge Conflict in main.py after Merging Development

--- src/backend/text_extractor.py
+++ src/backend/text_extractor.py
@@ -5,6 +5,15 @@ import pytesseract
 from pdf2image import convert_from_path
 from PIL import Image
 
+
+from io import BytesIO
+from pdf2image import convert_from_bytes
+import json
+import re
+import csv
+
+# OLD ---------------------------------------------------------------------------------------------------
+
 # Supported file types
 IMAGE_EXTS = {".jpg", ".jpeg", ".png", ".gif"}
 PDF_EXTS = {".pdf"}
@@ -52,3 +61,31 @@ def read_text_file(file_path: str) -> str:
         print(f"Text file read failed: {e}")
         return ""
 
+# ---------------------------------------------------------------------------------------------------
+
+def extract_text_from_bytes(file_bytes: bytes, filename: str) -> str:
+    """ Extracts readable text from in memory bytes """
+    ext = Path(filename).suffix.lower()
+
+    # Text-based formats
+    if ext in [".txt", ".csv", ".json", ".xml"]:
+        text = file_bytes.decode("utf-8", errors="ignore")      # Converts bytes into readable text
+        if ext == ".xml":
+            cleaned = re.sub(r">\s+<", "><", text)
+            return cleaned.strip()
+        elif ext == ".csv":
+            return "\n".join(
+                [" ".join(row) for row in csv.reader(text.splitlines())]
+            )
+        return text.strip()
+
+    elif ext == ".pdf":
+        pages = convert_from_bytes(file_bytes)
+        text = ""
+        for page in pages:
+            text += pytesseract.image_to_string(page)
+        return text.strip()
+
+    elif ext in [".jpg", ".jpeg", ".png", ".gif"]:
+        image = Image.open(BytesIO(file_bytes))
+        return pytesseract.image_to_string(image).strip()
